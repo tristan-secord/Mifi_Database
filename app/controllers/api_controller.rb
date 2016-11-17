ae469efc7652997031d56af7389df917079f9044
tristan-secord <tristan.secord@hotmail.com>
2016-11-17 15:53:58 -0500

    Debug for test function

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
 	http_basic_authenticate_with name:ENV["API_AUTH_NAME"], password:ENV["API_AUTH_PASSWORD"], :only => [:signup, :signin, :get_token]  
-	before_filter :check_for_valid_authtoken, :except => [:signup, :signin, :get_token]
+	before_filter :check_for_valid_authtoken, :except => [:signup, :signin, :get_token, :test]
 
 	def signup
 		if request.post?
