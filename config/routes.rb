23c515e5712a3a3986e0e9ce26ff30b7cadf3db9
tristan-secord <tristan.secord@hotmail.com>
2016-11-17 14:30:04 -0500

    Testing connection to 1&1 Domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'api/test'
+
+  match "*path", to: "application#page_not_found", via: :all
 end
