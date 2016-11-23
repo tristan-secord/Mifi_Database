643dc33c8815adbe93308138a119927f7332f34f
tristan-secord <tristan.secord@hotmail.com>
2016-11-23 15:15:29 -0500

    Protect from forgery with null session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+
+  def page_not_found
+    e = Error.new(:status => 404, :message => "Wrong URL or HTTP method")    
+    render :json => e.to_json, :status => 404
+  end
 end
