322ef9812de3160ea98e49690d964b552fbeca51
tristan-secord <tristan.secord@hotmail.com>
2016-11-24 15:51:20 -0500

    Debug Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post 'api/signup'
+  post 'api/signin'
 
   match "*path", to: "application#page_not_found", via: :all
 end
